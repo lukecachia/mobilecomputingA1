23a8e31bc7556c10bfc1ef01b0499bdc9d3833a5
lukecachia <lukecachia@gmail.com>
2019-10-22 09:49:08 +0200

    Create a directy which holds jquery/js functions. The panel was added as variable which is showed by a jquery function in order to avoid replicated HTML code. The goals HTML file has been removed and the code has been integrated in the index HTML file

---
+++ js/jq.js
@@ -0,0 +1,20 @@
+
+var panel = '<div id="nav-panel" data-role="panel" data-display="push" data-theme="b" class="ui-panel ui-panel-position-left ui-panel-display-push ui-body-b ui-panel-animate ui-panel-open">' +
+                '<div class="ui-panel-inner">' +
+                    '<ul data-role="listview" class="ui-listview">' +
+                        '<li class="ui-btn ui-icon-home ui-btn-icon-right ui-shadow ui-corner-all"><a href="#indexPage">Home</a></li>' +
+                        '<li><a href="#goalsPage">Goals</a></li>' + 
+                        '<li>Mock Ups</li>' + 
+                        '<li>Comparisons</li>' +
+                        '<li>Challanges</li>' +
+                        '<li>Personas</li>' +
+                    '</ul>' +
+                '</div>' +
+            '</div>'; 
+
+
+$(document).one('pagebeforecreate', function() {
+    $.mobile.pageContainer.prepend(panel);
+    $("#nav-panel").panel().enhanceWithin();
+
+})
